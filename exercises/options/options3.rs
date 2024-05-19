09db680f6ccfe01c74917fb25ea031bdd1cbead1
the sun <2814239559@qq.com>
2024-05-19 12:07:05 +0800

    update

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -13,8 +13,8 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
-        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+    match &y {
+        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint traits1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 trait AppendBar {
     fn append_bar(self) -> Self;
@@ -15,6 +15,10 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for type `String`.
+    fn append_bar(self) -> Self {
+        // 将 "Bar" 追加到字符串末尾并返回结果
+        self + "Bar"
+    }
 }
 
 fn main() {
